e790e03e8de7d507ee9dad09b3e44ccc3f64e6b4
Abir Taheer <abir@taheer.me>
2021-04-18 08:56:52 -0400

    added simple test for server

---
+++ test/app.js
@@ -0,0 +1,13 @@
+const app = require('./../app');
+
+describe('App', function () {
+  it('should start without error', done => {
+    let server;
+    try {
+      server = app.listen(3000);
+      done();
+    } finally {
+      server.close();
+    }
+  });
+});
