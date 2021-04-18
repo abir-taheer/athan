86233eed9aa608107de8f53b704d8c062226255f
Abir Taheer <abir@taheer.me>
2021-04-18 09:31:54 -0400

    installed prettier and prettified files

--- test/app.js
+++ test/app.js
@@ -1,7 +1,7 @@
-const app = require('./../app');
+const app = require("./../app");
 
-describe('App', function () {
-  it('should start without error', done => {
+describe("App", function () {
+  it("should start without error", (done) => {
     let server;
     try {
       server = app.listen(3000);
